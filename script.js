3f5361e790f13c62ded098ec2b6d7a78e881cee5
Rahul Kumar Pandit <55670332+rahulfrndz02@users.noreply.github.com>
2023-08-03 22:39:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,3 +6,32 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+// toggle.addEventListener("click",(event)=>{
+// 	video.play() ? video.pause() : video.play();
+// });
+
+toggle.addEventListener("click", playPauseMedia);
+video.addEventListener("click", playPauseMedia);
+player.addEventListener("click", playPauseMedia);
+progress.addEventListener("click", handleProgress);
+function playPauseMedia() {
+  if (video.paused) {
+    //play.setAttribute("data-icon", "u");
+    video.play();
+  } else {
+    //play.setAttribute("data-icon", "P");
+    video.pause();
+  }
+}
+
+skipButtons.addEventListener("click", skip);
+function skip() {
+  const skipAmount = parseFloat(this.dataset.skip);
+  video.currentTime += skipAmount;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
